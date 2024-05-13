836a1ff7c56be0fa1140790fc56e8fd495ee73cc
igor raphael <169068315+igorraphael08@users.noreply.github.com>
2024-05-13 18:46:23 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("@@@@@@@@@@@@@@@@@@@");
+    System.out.println("@Série: The Hollow@");
+     System.out.println("@@@@@@@@@@@@@@@@@@@");
+     System.out.println("--------------------------");
+    System.out.println("Sinopse: Três adolescentes acordam em um mundo estranho sem memória de quem são ou como chegaram lá. Eles embarcam em uma jornada para casa, mas descobrem que estão sendo perseguidos por criaturas sinistras.");
+     System.out.println("---------------------------");
+    System.out.println("link do trailer: https://www.youtube.com/watch?v=XLA_uqAfbew");
+  }
+
+  
+}
